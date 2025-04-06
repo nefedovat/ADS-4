840f4a9d6a1d8838d7a200d1552dc2c87570ebb7
nefedovat <ntanya54321@gmail.com>
2025-04-06 16:26:59 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,59 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  for (int i = 0; i < len; ++i) {
+    for (int j = i + 1; j < len; ++j) {
+      if (arr[i] + arr[j] == value) {
+        count++;
+      }
+    }
+  }
+  return count;
 }
 int countPairs2(int *arr, int len, int value) {
+  int left = 0;
+  int right = len - 1;
+  int count = 0;
+  while (left < right) {
+    int sum = arr[left] + arr[right];
+    if (sum == value) {
+      count++;
+      left++;
+      right--;
+    }
+    else if (sum < value) {
+      left++;
+    }
+    else {
+      right--;
+    }
+  }
+  return count;
+}
+
+int binarySearch(int* arr, int left, int right, int target) {
+  while (left <= right) {
+    int mid = left + (right - left) / 2;
+    if (arr[mid] == target) {
+      return 1;
+    }
+    else if (arr[mid] < target) {
+      left = mid + 1;
+    }
+    else {
+      right = mid - 1;
+    }
+  }
   return 0;
 }
+
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+  int count = 0;
+  for (int i = 0; i < len; ++i) {
+    int complement = value - arr[i];
+    if (binarySearch(arr, i + 1, len - 1, complement)) {
+      count++;
+    }
+  }
+  return count;
 }
