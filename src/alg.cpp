7eab956f1e46e48be3f2bed028d52b35ed366df1
nefedovat <ntanya54321@gmail.com>
2025-04-06 17:34:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,11 +1,10 @@
+// Copyright 2021 NNTU-CS
 int countPairs1(int* arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; ++i) {
     for (int j = i + 1; j < len; ++j) {
       if (arr[i] + arr[j] == value) {
-        count++;
-      } else if (arr[i] + arr[j] > value) {
-        break;
+        ++count;
       }
     }
   }
@@ -13,14 +12,13 @@ int countPairs1(int* arr, int len, int value) {
 }
 
 int countPairs2(int* arr, int len, int value) {
-  int count = 0;
   int left = 0, right = len - 1;
+  int count = 0;
   while (left < right) {
     int sum = arr[left] + arr[right];
     if (sum == value) {
       count++;
       left++;
-      right--;
     } else if (sum < value) {
       left++;
     } else {
@@ -30,12 +28,14 @@ int countPairs2(int* arr, int len, int value) {
   return count;
 }
 
-int binarySearch(int* arr, int start, int end, int target) {
-  while (start <= end) {
-    int mid = start + (end - start) / 2;
+int binarySearch(int* arr, int left, int right, int target) {
+  while (left <= right) {
+    int mid = (left + right) / 2;
     if (arr[mid] == target) return 1;
-    if (arr[mid] < target) start = mid + 1;
-    else end = mid - 1;
+    if (arr[mid] < target)
+      left = mid + 1;
+    else
+      right = mid - 1;
   }
   return 0;
 }
@@ -43,8 +43,8 @@ int binarySearch(int* arr, int start, int end, int target) {
 int countPairs3(int* arr, int len, int value) {
   int count = 0;
   for (int i = 0; i < len; ++i) {
-    int needed = value - arr[i];
-    if (binarySearch(arr, i + 1, len - 1, needed)) {
+    int target = value - arr[i];
+    if (binarySearch(arr, i + 1, len - 1, target)) {
       count++;
     }
   }
